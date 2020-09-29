7c8777f7971b24c40f5845962c010741fa032615
Abhishek Chawda <abhishek.mali@myntra.com>
2020-09-29 16:52:58 +0530

    update menu-items with navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Route} from 'react-router-dom'
 import './App.css';
 import Homepage from "./Pages/Homepage"
 function App() {
   return (
     <div >
       <h1>Hello World</h1>
-      <Homepage />
+      <Route exact path='/'><Homepage /></Route>
+      <Route exact path='/shop/hats'></Route>
     </div>
   );
 }
