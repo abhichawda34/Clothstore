72eec6199a62ef3ff219c9a10fed6aa1e0825f98
Abhishek Chawda <abhishek.mali@myntra.com>
2020-09-29 13:06:34 +0530

    broken homepage into components and styling of menu-items

---
+++ src/Pages/Homepage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './homepage.scss'
+import Directory from "../../Components/Directory"
+
+const Homepage = () =>{
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    )
+}
+
+export default Homepage
